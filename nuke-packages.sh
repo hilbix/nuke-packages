3dfbaf9569f4ef9cbb0fdbfe378e4904e8fa242f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2022-10-11 16:02:28 +0200

    nuke-packages.sh modified

--- nuke-packages.sh
+++ nuke-packages.sh
@@ -37,5 +37,5 @@ done >> "$TMP/debian/control" || exit
 find "$TMP" -print0 | xargs --null -- touch -d '1970-01-01 00:00:00 +0000' --
 echo building .. >&2
 
-cd "$TMP" && dpkg-buildpackage --unsigned-changes --unsigned-source --no-check-builddeps -b
+cd "$TMP" && dpkg-buildpackage --unsigned-changes --unsigned-source --no-check-builddeps
 
